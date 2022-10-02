2a89b36c46d423327838cc7d11d9f1f9b7f24e6c
Iku Iwasa <iku.iwasa@gmail.com>
2022-10-02 18:36:50 +0900

    test: Add tests for successful JSON parse cases

--- flymake-textlint-tests.el
+++ flymake-textlint-tests.el
@@ -40,5 +40,37 @@ https://github.com/textlint/textlint/blob/master/docs/configuring.md
 ")
     (should (not (flymake-textlint--parse-buffer "dummy")))))
 
+(ert-deftest flymake-textlint--parse-buffer-1-error ()
+  "Test whether 1 error output can be parsed."
+  (with-temp-buffer
+    (insert "[{\"messages\":[{\"type\":\"lint\",\"ruleId\":\"jtf-style/2.2.1.ひらがなと漢字の使い分け\",\"message\":\"又は => または\",\"index\":59,\"line\":3,\"column\":2,\"range\":[59,60],\"loc\":{\"start\":{\"line\":3,\"column\":2},\"end\":{\"line\":3,\"column\":3}},\"severity\":2,\"fix\":{\"range\":[59,61],\"text\":\"または\"}}],\"filePath\":\"/tmp/test.md\"}]
+")
+    (let ((diags (flymake-textlint--parse-buffer "dummy")))
+      (should (equal (length diags) 1))
+      (let ((diag (elt diags 0)))
+        (should (equal (flymake--diag-beg diag) 60))
+        (should (equal (flymake--diag-end diag) 61))
+        (should (equal (flymake--diag-type diag) :error))
+        (should (equal (flymake--diag-text diag) "又は => または"))))))
+
+(ert-deftest flymake-textlint--parse-buffer-2-error ()
+  "Test whether 2 errors output can be parsed."
+  (with-temp-buffer
+    (insert "[{\"messages\":[{\"type\":\"lint\",\"ruleId\":\"jtf-style/1.1.1.本文\",\"message\":\"本文を敬体(ですます調)に統一して下さい。\\n本文の文体は、敬体(ですます調)あるいは常体(である調)のどちらかで統一します。\\n\\\"である。\\\"が常体(である調)です。\",\"index\":66,\"line\":3,\"column\":9,\"range\":[66,67],\"loc\":{\"start\":{\"line\":3,\"column\":9},\"end\":{\"line\":3,\"column\":10}},\"severity\":2},{\"type\":\"lint\",\"ruleId\":\"jtf-style/2.2.1.ひらがなと漢字の使い分け\",\"message\":\"又は => または\",\"index\":73,\"line\":5,\"column\":2,\"range\":[73,74],\"loc\":{\"start\":{\"line\":5,\"column\":2},\"end\":{\"line\":5,\"column\":3}},\"severity\":2,\"fix\":{\"range\":[73,75],\"text\":\"または\"}}],\"filePath\":\"/tmp/test.md\"}]
+")
+    (let ((diags (flymake-textlint--parse-buffer "dummy")))
+      (should (equal (length diags) 2))
+      (let ((diag1 (elt diags 0))
+            (diag2 (elt diags 1)))
+        (should (equal (flymake--diag-beg diag1) 67))
+        (should (equal (flymake--diag-end diag1) 68))
+        (should (equal (flymake--diag-type diag1) :error))
+        (should (equal (flymake--diag-text diag1) "本文を敬体(ですます調)に統一して下さい。\n本文の文体は、敬体(ですます調)あるいは常体(である調)のどちらかで統一します。\n\"である。\"が常体(である調)です。"))
+        (should (equal (flymake--diag-beg diag2) 74))
+        (should (equal (flymake--diag-end diag2) 75))
+        (should (equal (flymake--diag-type diag2) :error))
+        (should (equal (flymake--diag-text diag2) "又は => または"))
+        ))))
+
 (provide 'flymake-textlint-tests)
 ;;; flymake-textlint-tests.el ends here
